81ef0848c7c2617e06015f8071342aa60a39a32c
Alexandra Dorey <adorey@ourmagnet.net>
2023-12-09 00:39:58 -0500

    aoc day 9

---
+++ Day09.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2023;
+public static class Day09
+{
+    private static long asI(this string input) => long.Parse(input);
+
+    public static string step1()
+    {
+        var input = File.ReadAllLines("data\\aoc9.txt");
+
+        long total = 0;
+
+        foreach (var line in input)
+        {
+            var ssp = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            List<long> nums = new();
+            foreach (var num in ssp)
+            {
+                nums.Add(num.asI());
+            }
+
+            res(nums);
+            total += nums.Last();
+        }
+
+        return total.ToString();
+    }
+
+    public static string step2()
+    {
+        var input = File.ReadAllLines("data\\aoc9.txt");
+
+        long total = 0;
+
+        foreach (var line in input)
+        {
+            var ssp = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            List<long> nums = new();
+            foreach (var num in ssp)
+            {
+                nums.Add(num.asI());
+            }
+
+            nums.Reverse();
+
+            res(nums);
+            total += nums.Last();
+        }
+
+        return total.ToString();
+    }
+
+    static void res(List<long> nums)
+    {
+        bool allzero = true;
+        for (int x = 0; x < nums.Count; x++)
+        {
+            if (nums[x] != 0)
+            {
+                allzero = false;
+                break;
+            }
+        }
+
+        if (allzero == true)
+        {
+            nums.Add(0);
+        }
+        else
+        {
+            List<long> newnums = new();
+            for (int x = 0; x < nums.Count - 1; x++)
+            {
+                newnums.Add(nums[x+1] - nums[x]);
+            }
+            res(newnums);
+            nums.Add(nums.Last() + newnums.Last());
+        }
+    }
+}
+ 
--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine($"Day08: StepRage: {Day08.step2rage()}");
-        Console.WriteLine($"Day08: Step2: {Day08.step2()}");
+        
+        Console.WriteLine($"Day08: Step2: {Day09.step1()}");
     }
 }
  
